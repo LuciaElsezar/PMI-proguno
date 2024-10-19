6fb0cba8aaa801d53638e6eb1b0f2ee8149a0b4e
*LittleByte* <maria.villa11106@gmail.com>
2024-10-19 17:59:50 -0300

    modificaciones main
    Se agregaron los prototipos relacionados a listaD_turno

--- Proyecto de Maquina Integrador/main.c
+++ Proyecto de Maquina Integrador/main.c
@@ -18,13 +18,39 @@ Tratamiento tratamientos[10]= {{"Unas", 7400},{"Limpieza Facial", 9400},
                                 {"Ondas Galvanicas", 123},{"Botox", 123}};
 
 //---Funciones-------------------------------------------
-void CargaTurno();
-//b)Funcion auxiliar que busca los turnos no realizados
-void BuscaTurno();
-//c)Funcion que muestra los turnos de todo un mes
-void Buscar
+void carga_turno(Lista t);
+//b)---Funcion auxiliar que busca los turnos no realizados
+void buscar_turno(Lista t);
+//c)---Funcion que muestra los turnos de todo un mes ordenado por fecha
+void mostrar_turno_fecha(Lista t);
+//d)---Funcion que muestra los turno por nombre de cliente
+void mostrar_turno_nombre(Lista t);
+//e)---Mostrar los turnos solicitados en el mes por idCliente. Mostrar fecha total y idTurno(recursiva)
+void mostrar_turno_idCliente(Lista t);
+//f)---Calcula la ganancia mensual considerando los turnos ya realizados(recursiva)
+float ganancia_mensual(Lista t);
+//g)---Muestra los turnos de la lista de turnos
+void mostrar_lista_turnos(Lista t);
+//h)---Funcion que modifica la cantidad de tratamientos de un cliente registrado
+void modifica_tratamiento();
+//i)---Modifica la forma de pago de un turno segun idCliente(solo turnos no realizados)
+void modifica_formapago(Lista t);
+//j)---Cancelar turno por Id cliente. Detalles en el practico
+void cancelar_turno(Lista t);
+//l)---funcion que permite registrar un cliente
+void registar_cliente();
+//m)---Muestra los turnos por tratamiento
+void mostrar_turno_tratamiento(Lista t);
+//n)---¿¿??
 
-
+//ñ)---Muestra los clientes almacenados en lista clientes
+void mostrar_lista_clientes();
+//o)---Elimina un cliente (los turnos no quedan afectados)
+void eliminar_cliente();
+//p)---Funcion que modifica un turno confirmado (si asiste se modifica realizado, can tratamientos y el nivel)
+void modifica_cliente();
+//q)---Muestra los turnos no realizados
+void mostrar_turno_norealizado(Lista t);
 
 //---Main------------------------------------------------
 int main()
